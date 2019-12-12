e35fcd772a716843561d961cac94792a058bb09a
Tino Rusch <tino.rusch@gmail.com>
2019-12-12 10:27:53 +0100

    add seed option.

--- cmd/namegen/main.go
+++ cmd/namegen/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 
 	"github.com/trusch/namegen/pkg/generator"
 )
 
+var (
+	seed = flag.Int("seed", 0, "random seed to use")
+)
+
 func main() {
-	generator.Seed(time.Now().UnixNano())
+	flag.Parse()
+	if *seed > 0 {
+		generator.Seed(int64(*seed))
+	} else {
+		generator.Seed(time.Now().UnixNano())
+	}
 	name := generator.Generate()
 	fmt.Println(name)
 }
